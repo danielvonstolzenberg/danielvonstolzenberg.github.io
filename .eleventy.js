1ca1837e2c97f70aa5435dcc210eef8fa667ed7f
danielvonstolzenberg <128248392+danielvonstolzenberg@users.noreply.github.com>
2023-07-23 12:04:50 +0200

    docs now

--- .eleventy.js
+++ .eleventy.js
@@ -2,5 +2,10 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy('assets')
     return {
         passthroughFileCopy: true
+    },
+    module.exports = {
+        dir: {
+            output: "docs"
+        }
     }
  }
